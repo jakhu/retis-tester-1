1c414fce2ab92fa912e45b9151e20db3bee330c4
Gum-Joe <kishansambhi@hotmail.co.uk>
2016-05-20 18:23:52 +0100

    Update app.js
    Add logging

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 // Sample entry
 module.exports = (retis) => {
   retis.logger.info("Sample Plugin is working!")
+  retis.logger.info("Properties of the retis object:")
+  retis.logger.info(retis)
+  retis.logger.info("Config:")
+  retis.logger.info(retis.config)
 }
+
